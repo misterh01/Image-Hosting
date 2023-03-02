b71659eb43af071965140e9156d5de02b3e0f71c
misterh01 <hrashid4391@gmail.com>
2023-03-01 19:34:38 -0500

    ready for production

---
+++ README.md
@@ -0,0 +1,8 @@
+# Free Image Hosting Service
+---
+Free image jpeg, png, gif, webp, and svg hosting. No Login required!
+
+# [Open Website](https://imghosting.misterh.dev)
+---
+
+## Backend is written in Python. [Here's the link]()
---
+++ script.js
@@ -0,0 +1,74 @@
+const upload = new FileUploadWithPreview.FileUploadWithPreview('myFirstImage');
+const btn = document.querySelector(".btn");
+const error = document.querySelector(".error");
+const data = document.querySelector(".data");
+
+const allowedExtension = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/svg+xml",
+  "image/gif"
+]
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const file = upload.cachedFileArray[0]
+  if(!file) {
+    return
+  }
+
+  btn.disabled = true
+  btn.innerHTML = "<div class='spinner'></div>"
+
+  if (!isValidFileType(file.type)) {
+    error.innerHTML = "Invalid File Type!"
+    error.style.display = "block"
+
+    upload.resetPreviewPanel();
+    btn.disabled = false
+    btn.innerHTML = "Upload"
+
+
+    setTimeout(() => {
+      error.innerHTML = ""
+      error.style.display = "none"
+    }, 2600)
+
+    return
+  }
+
+  let formData = new FormData();
+  formData.append("file", file, file.name.split(":")[0])
+
+
+
+  const sendReq = async () => {
+    const req = await fetch('https://images.misterh.dev/upload', {
+      method: "POST",
+      body: formData
+    });
+
+    const res = await req.text()
+
+    if (res.startsWith("error")) {
+      error.innerHTML = "Error while trying to upload. Please try again!"
+      error.style.display = "block"
+    } else {
+      data.style.display = "flex"
+      data.firstElementChild.innerHTML = `https://images.misterh.dev/i/${res}`
+      data.firstElementChild.href = `https://images.misterh.dev/i/${res}`
+    }
+
+    btn.disabled = false
+    upload.resetPreviewPanel();
+    btn.innerHTML = "Upload"
+  }
+
+
+  sendReq()
+})
+
+function isValidFileType(type) {
+  return allowedExtension.includes(type)
+}
